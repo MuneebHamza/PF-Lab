2ca4a321e60d31443ce98d6f5e0f69bc8721d779
MuneebHamza <muneebhamza420420@gmail.com>
2026-02-21 14:16:40 +0500

    message

---
+++ week2/Task/task7_character.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+ main () {
+
+  cout<<"    _______       "<<endl;
+  cout<<"   /  ___  \\     "<<endl;
+  cout<<"  /  / @ \\ \\____"<<endl;
+  cout<<" |   \\___/     _|"<<endl;
+  cout<<" |          ____| "<<endl;
+  cout<<"  \\________/     "<<endl;
+
+
+}

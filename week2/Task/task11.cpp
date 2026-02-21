17675b1d693738573efac7782746915a930dd5a2
MuneebHamza <muneebhamza420420@gmail.com>
2026-02-21 10:55:48 +0500

    message

---
+++ week2/Task/task11.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+ main () {
+
+  cout << "         ____________________" << endl;
+ cout << "        /|_||_||_||_||_||_||_\\`.__" << endl;
+ cout << "       (   _    _     _    _     _)" << endl;
+    cout << "    =`-(_)--(_)--(_)--(_)--(_)--'" << endl;
+
+ }

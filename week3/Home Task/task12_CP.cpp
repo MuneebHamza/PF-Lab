73b6b72cbba6de8bd33f89f96c26fc0bc6b5a912
MuneebHamza <muneebhamza420420@gmail.com>
2026-02-28 13:29:16 +0500

    message

--- week3/Home Task/task12_CP.cpp
+++ week3/Home Task/task12_CP.cpp
@@ -1,6 +1,18 @@
-#inclue<iostream>
+#include<iostream>
 using namespace std;
 
  main () {
 
- 
+    int n;
+    cout<<"Number of square meters you can paint: ";
+    cin>>n;
+    int w;
+    cout<<"Widrh of the single wall (in meters): ";
+    cin>> w;
+    int h;
+    cout<<"Height of the single wall (in meters): ";
+    cin>>h;
+    int paint=n/(w*h);
+    cout<<"Number of walls you can paint: "<<paint;
+    
+}
---
+++ week3/Home Task/task13_CP.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+  main () {
+
+  int number,sum;
+  cout<<"Enter 5 integers: "<<endl;
+  cin>>number;
+  sum=number+sum;	
+  cin>>number;
+  sum=number+sum;
+  cin>>number;
+  sum=number+sum;
+  cin>>number;
+  sum=number+sum;
+  cin>>number;
+  sum=number+sum;
+  cout<<"Sum = "<<sum;
+ 
+}
